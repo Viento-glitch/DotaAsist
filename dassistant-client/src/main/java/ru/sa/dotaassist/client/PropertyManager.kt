31c7779774a6b228fcd3b64e4f7f39d4a2391e0c
swarm157 <yrakurbatov4@gmail.com>
2022-07-08 19:37:12 +0400

    Writing settings 2x

--- dassistant-client/src/main/java/ru/sa/dotaassist/client/PropertyManager.kt
+++ dassistant-client/src/main/java/ru/sa/dotaassist/client/PropertyManager.kt
@@ -30,14 +30,21 @@ class PropertyManager {
     private val file = File(path)
     private var properties = Properties()
     fun save() {
-
+        try {
+            properties.store(FileWriter(file), "")
+        } catch (e: Exception) {
+            println("ERROR: cannot save $path")
+        }
     }
     fun load() {
         try {
             if (file.isDirectory) {
                 if (!file.delete()) {
-                    println("ERROR: $path  is directory with files")
+                    println("ERROR: $path  is a directory with files")
                     properties = standardSettings()
+                } else {
+                    properties = standardSettings()
+                    properties.store(FileWriter(file), "")
                 }
             } else {
                 if (file.exists()) {
@@ -52,6 +59,11 @@ class PropertyManager {
             println("ERROR: cannot do anything with $path \n loading default settings in read only")
             properties = standardSettings()
         }
-
+    }
+    fun get(key: String): String? {
+        return properties.getProperty(key)
+    }
+    fun set(key: String, value: String) {
+        properties[key] = value
     }
 }
--- dassistant-client/src/main/java/ru/sa/dotaassist/client/RobotAssistant.kt
+++ dassistant-client/src/main/java/ru/sa/dotaassist/client/RobotAssistant.kt
@@ -3,11 +3,24 @@ package ru.sa.dotaassist.client
 import java.awt.Dimension
 import java.awt.Point
 import java.awt.Robot
+import java.awt.event.KeyEvent
 
 class RobotAssistant(
     val imgSize: Dimension,
     val imgPos: Point
 ) : Assistant() {
     val robot = Robot()
+
+    fun send(text: String) {
+        robot.apply {
+            keyPress(KeyEvent.VK_ENTER)
+            delay(10)
+            keyRelease(KeyEvent.VK_ENTER)
+        }
+    }
+
+    fun cut() {
+
+    }
 
 }
