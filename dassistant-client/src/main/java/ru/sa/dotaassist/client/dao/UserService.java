300b51255a6060d920d845d1488b0a147bc315b9
Viento <sterribilis@gmail.com>
2022-07-10 13:32:04 +0300

    moved files in proper folders

--- dassistant-client/src/main/java/ru/sa/dotaassist/client/dao/UserService.java
+++ dassistant-client/src/main/java/ru/sa/dotaassist/client/dao/UserService.java
@@ -1,7 +1,8 @@
-package ru.sa.dotaassist.client;
+package ru.sa.dotaassist.client.dao;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import ru.sa.dotaassist.client.UserApiResponse;
 
 public interface UserService {
     @GET("/api/users/{id}")
